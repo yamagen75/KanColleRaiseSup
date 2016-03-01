eedb29a870c0057d5d8b70936b545c2362ac8049
yamagen75 <yamagen_75@yahoo.co.jp>
2016-03-01 23:05:21 +0900

    ゲーム画面上部にあるDMMヘッダの非表示に再度対応
    昨日はHTML内でDMMヘッダのDIVに付いていたIDが消えていたが現状では昔の状態に戻っているのでIDの場合もクラス名の場合も対応出来るように修正した

--- content.js
+++ content.js
@@ -8,7 +8,18 @@ cont.style.backgroundColor = '#fff';
 cont.style.width = '650px';
 cont.style.height = '70px';
 cont.id = 'emorgp1';
-var hd_nvnm = 'dmm-ntgnavi';
+/* 操作するオブジェクト「DMMヘッダ」を取得する */
+var hd_nvnm = 'dmm-ntgnavi-renew';
+var contd = document.getElementById(hd_nvnm);
+if(contd == undefined){
+	hd_nvnm = 'dmm-ntgnavi';
+	var hd_tgs = document.getElementsByTagName('div');
+	for(var hd_i = 0;hd_i < hd_tgs.length;hd_i++){
+		if(hd_tgs[hd_i].className == hd_nvnm){
+			contd = hd_tgs[hd_i];		break;
+		}
+	}
+}
 var htma = '<div id="emorgp2" style="margin:5px;">';
 htma += '<button type="button" onClick="orgplopenclose(1);" style="margin:0 20px 5px 0;"><< 閉じる</button>';
 htma += '<strong>艦これの艦娘育成を支援する GoogleChrome拡張</strong>';
@@ -35,22 +46,17 @@ htmb += 'document.getElementById("emorgp4").style.display = fb; ';
 htmb += 'document.getElementById("emorgp5").style.display = fa; ';
 htmb += 'orgdmmhedelemntget("'+ hd_nvnm +'").style.display = fc; ';
 htmb += 'document.body.style.minWidth = fd; } function orgdmmhedelemntget(nm){ ';
+htmb += 'var hd = document.getElementById(nm); if(hd == undefined){ ';
 htmb += 'var tgs = document.getElementsByTagName("div"); ';
-htmb += 'for(var i = 0;i < tgs.length;i++){ if(tgs[i].className == nm){ return tgs[i]; } } }';
-/* 操作するオブジェクト(area-game,game_frame,DMMヘッダ)が取得出来れば実行する */
+htmb += 'for(var i = 0;i < tgs.length;i++){ if(tgs[i].className == nm){ return tgs[i]; } }';
+htmb += ' }else{ return hd; } }';
+/* 操作するオブジェクト「area-game」「game_frame」を取得する */
 var contb = document.getElementById('area-game');
 var contc = document.getElementById('game_frame');
-var contd = null;
-var hd_tgs = document.getElementsByTagName('div');
-for(var hd_i = 0;hd_i < hd_tgs.length;hd_i++){
-	if(hd_tgs[hd_i].className == hd_nvnm){
-		contd = hd_tgs[hd_i];		break;
-	}
-}
 var htmc = '';
 if(contb == undefined) htmc += '<div>area-gameが取得出来ません。</div>';
 if(contc == undefined) htmc += '<div>game_frameが取得出来ません。</div>';
-if(contd == null) htmc += '<div>'+ hd_nvnm +'が取得出来ません。</div>';
+if(contd == undefined) htmc += '<div>'+ hd_nvnm +'が取得出来ません。</div>';
 if(htmc == ''){
 	contb.style.textAlign = 'left';		contb.style.marginTop = '5px';
 	contd.style.display = 'none';		contc.width = '840px';
