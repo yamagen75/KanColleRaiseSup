f9b42aaa0fb4399422f21c7a4d921d84f17aac4c
skywalker999u <skywalker999u@gmail.com>
2017-02-18 02:14:11 +0900

    墳式強襲(基地航空隊)の仮対応
    基地航空隊に墳式機を配備した際に発生する墳式強襲（ api_air_base_injection_kouku ）への仮対応

--- devtools.js
+++ devtools.js
@@ -2327,7 +2327,8 @@ function on_battle(json, battle_api_name) {
 		var t0 = d.api_flare_pos[0]; if (t0 != -1) result.detail.push({ty:'照明弾(夜戦)', at: nowhps_c ? t0+20 : t0});
 		var t1 = d.api_flare_pos[1]; if (t1 != -1) result.detail.push({ty:'敵照明弾(夜戦)', at:t1+6});
 	}
-	calc_kouku_damage(result, nowhps, d.api_injection_kouku, nowhps_c); // 航空戦.
+	calc_kouku_damage(result, nowhps, d.api_air_base_injection_kouku, nowhps_c); // 噴式強襲(基地航空隊).
+	calc_kouku_damage(result, nowhps, d.api_injection_kouku, nowhps_c); // 噴式強襲.
 	if (d.api_air_base_attack) {
 		d.api_air_base_attack.forEach(function(kouku) {
 			calc_kouku_damage(result, nowhps, kouku, nowhps_c);　// 2016.5 基地航空隊支援.
