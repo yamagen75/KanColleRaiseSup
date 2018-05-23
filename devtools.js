572539f733d2dac5a53cc41169134485411852b3
SkyRavenHK <skywalker999u@hotmail.com>
2018-05-23 09:46:52 +0800

    Fix: typo

--- devtools.js
+++ devtools.js
@@ -2363,7 +2363,7 @@ function on_next_cell(json) {
 		var msg = area;
 		msg += ':揚陸地点';
 		$battle_log.push(msg);
-		print_next('next event', msg);
+		dpnla.tmpviw(1,'c41',arow +'Event '+ msg);
 	}
 	else {	// 戦闘マス.
 		var i = 0;	var ky = 't42';		var ha = '';	var rb = ['m'];		var tb = dpnla.tmpget('tp4_3');
