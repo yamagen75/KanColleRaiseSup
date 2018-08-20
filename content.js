55a3b24421aa686c195bd5c235407466cee9297c
yamagen75 <yamagen75@users.noreply.github.com>
2018-08-20 14:38:49 +0900

    20pxの余白を10pxに変更(10pxでも赤城さんのお風呂タイマーが取得出来た為)。 さらに余白を0pxに出来るボタンを追加。余白の状態はlocalStorageに保存し次回起動時に状態を復元します。

--- content.js
+++ content.js
@@ -2,10 +2,10 @@
 var cont = document.createElement('div');
 cont.style.whiteSpace = 'pre-wrap';
 cont.style.position = 'absolute';
-cont.style.top = '870px';
+cont.style.top = '860px';
 cont.style.left = '0px';
 cont.style.backgroundColor = '#fff';
-cont.style.width = '670px';
+cont.style.width = '770px';
 cont.style.height = '80px';
 cont.id = 'emorgp1';
 /* 操作するオブジェクト「DMMヘッダ」を取得する */
@@ -21,13 +21,19 @@ if(contd == undefined){
 		}
 	}
 }
+/* 設定読込 */
+var flgdef = { topleftfg:1 };
+var layoutvl = localStorage['orglayoutsetup'];
+var layoutfg = layoutvl ? JSON.parse(layoutvl) : flgdef;
+var layoutbn = '左上余白を0pxにする';
+if(layoutfg.topleftfg < 1) layoutbn = '左上余白を10pxにする';
 var htma = '<div id="emorgp2" style="margin:5px;">';
-htma += '<button type="button" onClick="orgplopenclose(1);" style="margin:0 20px 5px 0;"><< 閉じる</button>';
-htma += '<strong>艦これの艦娘育成を支援する GoogleChrome拡張</strong>';
-htma += '<button type="button" id="emorgp4" onClick="orgdmmhedonoff(1);" ';
-htma += 'style="margin:0 0 5px 20px;">DMM.com ヘッダー表示</button>';
-htma += '<button type="button" id="emorgp5" onClick="orgdmmhedonoff(0);" ';
-htma += 'style="margin:0 0 5px 20px;display:none;">DMM.com ヘッダー非表示</button>';
+htma += '<button type="button" onClick="orgplopenclose(1);" style="margin:0 20px 5px 0;">';
+htma += '< 閉じる</button><strong>艦これの艦娘育成を支援する GoogleChrome拡張</strong>';
+htma += '<button type="button" id="emorgp4" onClick="orgdmmhedonoff();" ';
+htma += 'style="margin:0 0 5px 20px;">DMM.com ﾍｯﾀﾞｰ･ﾌｯﾀｰ表示</button>';
+htma += '<button type="button" id="emorgp5" onClick="orglayoutsetflgsave();" ';
+htma += 'style="margin:0 0 5px 20px;">'+ layoutbn +'</button>';
 htma += '<div>デベロッパーツールを、Opt+Cmd+I(Mac)，Ctrl+Shift+I または F12 キーを押して起動してください。';
 htma += '</div><div style="margin-top:2px;">デベロッパーツールに「艦これ情報」のタブが追加されない場合';
 htma += 'には、デベロッパーツールを起動し直してください。</div></div>';
@@ -35,40 +41,58 @@ htma += '<div id="emorgp3" style="display:none;">';
 htma += '<button type="button" onClick="orgplopenclose(0);" ';
 htma += 'style="margin:5px 0 0 0;padding:0;">開<br />く<br />></button></div>';
 var scrp = document.createElement('script');
-var htmb = 'function orgplopenclose(pt){ var fa = "block"; var fb = "none"; var fc = "670px"; ';
+var htmb = 'function orgplopenclose(pt){ var fa = "block"; var fb = "none"; var fc = "770px"; ';
 htmb += 'if(pt > 0){ fa = "none"; fb = "block"; fc = "18px"; } ';
 htmb += 'document.getElementById("emorgp1").style.width = fc; ';
 htmb += 'document.getElementById("emorgp2").style.display = fa; ';
-htmb += 'document.getElementById("emorgp3").style.display = fb;';
-htmb += ' } function orgdmmhedonoff(pt){ var fa = "none"; var fb = "inline-block"; var fc = "none"; ';
-htmb += 'if(pt > 0){ fa = "inline-block"; fb = "none"; fc = "block"; } ';
-htmb += 'document.getElementById("emorgp4").style.display = fb; ';
-htmb += 'document.getElementById("emorgp5").style.display = fa; ';
-htmb += 'orgdmmhedelemntget("'+ hd_nvnm +'").style.display = fc;';
-htmb += ' } function orgdmmhedelemntget(nm){ ';
-htmb += 'var hd = document.getElementById(nm); if(hd == undefined){ ';
-htmb += 'var tgs = document.getElementsByTagName("div"); ';
-htmb += 'for(var i = 0;i < tgs.length;i++){ var cnm = tgs[i].className;';
-htmb += ' if(cnm.indexOf(nm) >= 0){ return tgs[i]; } }';
-htmb += ' }else{ return hd; } }';
-/* 操作するオブジェクト「area-game」「main-ntg」「foot」を取得する */
+htmb += 'document.getElementById("emorgp3").style.display = fb; } ';
+htmb += 'function orgdmmhedonoff(){ var ho = orgdmmhedelemntget("'+ hd_nvnm +'"); ';
+htmb += 'var fo = orgdmmhedelemntget("area-naviapp"); var fa = "block"; ';
+htmb += 'var bn = document.getElementById("emorgp4"); var fb = "DMM.com ﾍｯﾀﾞｰ･ﾌｯﾀｰ非表示"; ';
+htmb += 'if(ho.style.display != "none"){ fa = "none"; fb = "DMM.com ﾍｯﾀﾞｰ･ﾌｯﾀｰ表示"; } ';
+htmb += 'ho.style.display = fa; fo.style.display = fa; bn.innerHTML = fb; } ';
+htmb += 'function orglayoutsetflgsave(){ var fb = "左上余白を10pxにする"; ';
+htmb += 'var ga = document.getElementById("area-game"); var fa = "0px"; ';
+htmb += 'var bn = document.getElementById("emorgp5"); var vl = { topleftfg:0 }; ';
+htmb += 'if(ga.style.marginTop != "10px"){ fa = "10px"; fb = "左上余白を0pxにする"; vl.topleftfg = 1; } ';
+htmb += 'ga.style.marginTop = fa; ga.style.marginLeft = fa; bn.innerHTML = fb; ';
+htmb += 'localStorage["orglayoutsetup"] = JSON.stringify(vl); } ';
+htmb += 'function orgdmmhedelemntget(nm){ var hd = document.getElementById(nm); ';
+htmb += 'if(hd == undefined){ var tgs = document.getElementsByTagName("div"); ';
+htmb += 'for(var i = 0;i < tgs.length;i++){ var cnm = tgs[i].className; ';
+htmb += 'if(cnm.indexOf(nm) >= 0){ return tgs[i]; } } }else{ return hd; } }';
+/* 操作するオブジェクト「area-game」「main-ntg」「foot」「area-naviapp」を取得する */
 var contb = document.getElementById('area-game');
 var contc = document.getElementById('main-ntg');
 var conte = document.getElementById('foot');
+var contf = undefined;
+var ft_tgs = document.getElementsByTagName('div');
+for(var ft_i = 0;ft_i < ft_tgs.length;ft_i++){
+	var ft_clnm = ft_tgs[ft_i].className;
+	if(ft_clnm.indexOf('area-naviapp') >= 0){
+		contf = ft_tgs[ft_i];		break;
+	}
+}
 var htmc = '';
 if(contb == undefined) htmc += '<div>area-gameが取得出来ません。</div>';
 if(contc == undefined) htmc += '<div>main-ntgが取得出来ません。</div>';
 if(contd == undefined) htmc += '<div>'+ hd_nvnm +'が取得出来ません。</div>';
 if(conte == undefined) htmc += '<div>footが取得出来ません。</div>';
+if(contf == undefined) htmc += '<div>area-naviappが取得出来ません。</div>';
 if(htmc == ''){
 	contb.style.textAlign = 'left';
-	contb.style.marginLeft = '20px';
-	contb.style.marginTop = '4px';
+	var layoutfa = '10px';
+	if(layoutfg.topleftfg < 1) layoutfa = '0px';
+	contb.style.marginTop = layoutfa;
+	contb.style.marginLeft = layoutfa;
 	contd.style.display = 'none';
 	conte.style.display = 'none';
+	contf.style.display = 'none';
 	contc.style.paddingBottom = '0px';
-	scrp.innerHTML = htmb;	document.head.appendChild(scrp);
+	scrp.innerHTML = htmb;
+	document.head.appendChild(scrp);
 }else{
 	htma = htmc;
 }
-cont.innerHTML = htma;	document.body.appendChild(cont);
+cont.innerHTML = htma;
+document.body.appendChild(cont);
--- contsub.js
+++ contsub.js
@@ -12,18 +12,47 @@ function getifmsrc(){
 		}
 	}
 	if(fg > 0){
+		var st = document.getElementById('spacing_top');
+		if(st != undefined) st.style.display = 'none';
+		var gn = document.getElementById('globalNavi');
+		if(gn != undefined) gn.style.display = 'none';
 		var sp = document.createElement('script');
 		ht = 'function subwinopen(){ window.open("'+ gu.replace(/&amp;/g,'&');
 		ht += '","kancollesubwin","width=1200,height=727"); ';
-		ht += 'document.getElementById("emorgb1").style.display = "none"; ';
-		ht += 'document.getElementById("flashWrap").innerHTML = ""; }';
-		sp.innerHTML = ht;	document.head.appendChild(sp);
-		var ba = document.createElement('button');	ba.style.margin = '5px';
-		ba.innerHTML = '艦これをサブウィンドウで開く';	ba.setAttribute('onclick','subwinopen();');
-		var bp = document.createElement('div');		bp.id = 'emorgb1';
-		bp.style.whiteSpace = 'pre-wrap';	bp.style.position = 'absolute';	bp.style.backgroundColor = '#fff';
-		bp.style.top = '770px';	bp.style.left = '480px';	bp.style.width = '220px';	bp.style.height = '37px';
-		bp.appendChild(ba);		document.body.appendChild(bp);
+		ht += 'document.getElementById("emorgb2").style.display = "none"; ';
+		ht += 'document.getElementById("flashWrap").innerHTML = ""; } ';
+		ht += 'function globalnavionoff(){ var gn = document.getElementById("globalNavi"); var fa = "block"; ';
+		ht += 'var bn = document.getElementById("emorgb3"); var fb = "ﾅﾋﾞｹﾞｰｼｮﾝ非表示"; ';
+		ht += 'if(gn.style.display != "none"){ fa = "none"; fb = "ﾅﾋﾞｹﾞｰｼｮﾝ表示"; } ';
+		ht += 'gn.style.display = fa; bn.innerHTML = fb; } ';
+		ht += 'function orgbtnopenclose(pt){ var fa = "block"; var fb = "none"; var fc = "195px"; ';
+		ht += 'if(pt > 0){ fa = "none"; fb = "block"; fc = "18px"; } ';
+		ht += 'document.getElementById("emorgb1").style.width = fc; ';
+		ht += 'document.getElementById("emorgb4").style.display = fa; ';
+		ht += 'document.getElementById("emorgb5").style.display = fb; }';
+		sp.innerHTML = ht;
+		document.head.appendChild(sp);
+		ht = '<div id="emorgb4"><div style="margin:0 0 5px 0;" id="emorgb2">';
+		ht += '<button type="button" onClick="subwinopen();">艦これをｻﾌﾞｳｨﾝﾄﾞｳで開く</button></div>';
+		ht += '<div><button type="button" onClick="orgbtnopenclose(1);" style="margin:0 5px 0 0;">';
+		ht += '< 閉じる</button>';
+		ht += '<button type="button" id="emorgb3" onClick="globalnavionoff();">';
+		ht += 'ﾅﾋﾞｹﾞｰｼｮﾝ表示</button></div></div>';
+		ht += '<div id="emorgb5" style="display:none;">';
+		ht += '<button type="button" onClick="orgbtnopenclose(0);" style="margin:0;padding:0;">';
+		ht += '開<br />く<br />></button></div>';
+		var bp = document.createElement('div');
+		bp.id = 'emorgb1';
+		bp.style.whiteSpace = 'pre-wrap';
+		bp.style.position = 'absolute';
+		bp.style.backgroundColor = '#fff';
+		bp.style.top = '745px';
+		bp.style.left = '0px';
+		bp.style.width = '195px';
+		bp.style.height = '65px';
+		bp.style.textAlign = 'left';
+		bp.innerHTML = ht;
+		document.body.appendChild(bp);
 	}else{
 		window.setTimeout(getifmsrc,1000);
 	}
